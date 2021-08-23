597542072b34bf61a74c63e0d1c87c2dd534356b
widal001 <williamdaly422@gmail.com>
2021-08-23 12:48:41 -0400

    Replaces MSAL with azure identity package to simplify code

--- src/aging_report/sharepoint/sharepoint.py
+++ src/aging_report/sharepoint/sharepoint.py
@@ -1,31 +1,38 @@
-import msal
 import requests
+from azure.core.credentials import AccessToken
+from azure.core.exceptions import ClientAuthenticationError
+from azure.identity import (
+    ClientSecretCredential,
+    CredentialUnavailableError,
+)
+
 
 from aging_report.config import settings
 
 
 class SharePoint:
+    """Creates a client to interface with SharePoint using Microsoft Graph API
+    and the Microsoft Authentication Library (MSAL)
+    """
+
     def __init__(self, config=settings):
 
         self.scopes = config.scopes
-        self.session = None
-
         try:
-            self.app = msal.ConfidentialClientApplication(
+            # instantiate a daemon app for authentication with azure identity
+            self.client = ClientSecretCredential(
+                config.tenant_id,
                 config.client_id,
-                authority=config.authority,
-                client_credential=config.client_secret,
+                config.client_secret,
             )
         except AttributeError as err:
             print("One of the config variables isn't set")
             raise err
 
-    def authenticate(self):
-
-        result = self.app.acquire_token_silent(self.scopes, account=None)
-        if not result:
-            result = self.app.acquire_token_for_client(self.scopes)
-        if "access_token" not in result:
-            print(result.get("error"))
-            print(result.get("error_description"))
-            print(result.get("correlation_id"))
+    def _authenticate(self) -> AccessToken:
+        """Find a cached access token or request a new one and return it"""
+        try:
+            token = self.client.get_token(self.scopes)
+        except (CredentialUnavailableError, ClientAuthenticationError) as err:
+            raise err
+        return token
--- tests/integration_tests/test_sharepoint.py
+++ tests/integration_tests/test_sharepoint.py
@@ -1,5 +1,3 @@
-import msal
-
 from aging_report.config import settings
 from aging_report.sharepoint import SharePoint
 
@@ -7,6 +5,5 @@ from aging_report.sharepoint import SharePoint
 class TestGraph:
     def test_authenticate_success(self):
         client = SharePoint()
-        client.authenticate()
-        result = client.app.acquire_token_silent(settings.scopes, account=None)
-        assert "access_token" in result
+        token = client._authenticate()
+        assert token is not None
