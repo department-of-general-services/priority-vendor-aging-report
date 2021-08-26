887095cfa9516a4d98ff86c79eb94d0ef6b75fed
widal001 <williamdaly422@gmail.com>
2021-08-26 10:45:06 -0400

    Adds python-o365 library to replace azure identity library

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from O365 import Account
 
 from aging_report.config import settings
 
@@ -10,3 +11,15 @@ def test_config():
     """Returns the configuration settings for use in tests"""
     test_settings = settings.from_env("testing")
     return test_settings
+
+
+@pytest.fixture(scope="session")
+def account():
+    credentials = (settings.client_id, settings.client_secret)
+    account = Account(
+        credentials,
+        auth_flow_type="credentials",
+        tenant_id=settings.tenant_id,
+    )
+    account.authenticate()
+    return account
--- tests/integration_tests/test_sharepoint.py
+++ tests/integration_tests/test_sharepoint.py
@@ -2,11 +2,13 @@ from pprint import pprint
 
 import pytest
 import requests
+from O365 import Account
 
 from aging_report.config import settings
 from aging_report.sharepoint import SharePoint, Site
 
 
+@pytest.mark.skip
 class TestSharePoint:
     """Tests the SharePoint class"""
 
@@ -21,6 +23,7 @@ class TestSharePoint:
         assert access_token.token
 
 
+@pytest.mark.skip
 class TestSite:
     """Tests the Site class"""
 
@@ -59,3 +62,39 @@ class TestSite:
         # validation
         assert response.status_code == 200
         assert "value" in data
+
+
+class TestO365:
+    @pytest.mark.skip
+    def test_authenticate(self):
+        # setup
+        credentials = (settings.client_id, settings.client_secret)
+        account = Account(
+            credentials,
+            auth_flow_type="credentials",
+            tenant_id=settings.tenant_id,
+        )
+        # execution
+        account.authenticate()
+        # validation
+        assert account.is_authenticated
+
+    @pytest.mark.skip
+    def test_get_site(self, account):
+        # setup
+        client = account.sharepoint()
+        # execution
+        site = client.get_site(settings.site_id)
+        # validation
+        assert site
+        assert "Fiscal" in site.display_name
+
+    def test_get_list_by_name(self, account):
+        # setup
+        client = account.sharepoint()
+        # execution
+        site = client.get_site(settings.site_id)
+        drives = site.list_document_libraries()
+        print(len(drives))
+        print(drives)
+        assert 0
