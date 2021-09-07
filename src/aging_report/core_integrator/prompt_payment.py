9f9adafc45437b81e95f8188c6fad7082a5ba062
widal001 <williamdaly422@gmail.com>
2021-09-07 15:21:57 -0400

    Adds file structure for core_integrator sub-package

---
+++ src/aging_report/core_integrator/prompt_payment.py
@@ -0,0 +1,12 @@
+class PromptPayment:
+    def __init__():
+        pass
+
+    def scrape():
+        pass
+
+    def archive():
+        pass
+
+    def update_report():
+        pass
---
+++ src/aging_report/core_integrator/scraper.py
@@ -0,0 +1,18 @@
+class CoreIntegrator:
+    def __init__():
+        pass
+
+    def scrape_report():
+        pass
+
+    def _login():
+        pass
+
+    def _access_report():
+        pass
+
+    def _rename_download():
+        pass
+
+    def _load_report():
+        pass
