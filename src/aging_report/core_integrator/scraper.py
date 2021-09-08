25a6bd259a2a339e1a53750997cb2599232ff8f5
widal001 <williamdaly422@gmail.com>
2021-09-08 09:24:31 -0400

    Adds docstrings to classes and methods

--- src/aging_report/core_integrator/scraper.py
+++ src/aging_report/core_integrator/scraper.py
@@ -1,18 +1,33 @@
+from __future__ import annotations  # prevents NameError for typehints
+
+
 class CoreIntegrator:
-    def __init__():
+    """Client for scraping data from the CoreIntegrator website"""
+
+    def __init__(self):
+        """Inits the CoreIntegrator class"""
         pass
 
-    def scrape_report():
+    def scrape_report(self) -> None:
+        """Scrapes and downloads the Prompt Payment report from CoreIntegrator
+        then loads it as a dataframe
+        """
         pass
 
-    def _login():
+    def _login(self):
+        """Logs into the CoreIntegrator website"""
         pass
 
-    def _access_report():
+    def _access_report(self):
+        """Searches for the Prompt Payment Report current through today's
+        date and then exports the report to Excel
+        """
         pass
 
-    def _rename_download():
+    def _rename_download(self):
+        """Confirms report was downloaded and renames it"""
         pass
 
-    def _load_report():
+    def _load_report(self):
+        """Loads the report as a dataframe"""
         pass
---
+++ src/aging_report/core_integrator/utils.py
@@ -0,0 +1,135 @@
+from __future__ import annotations  # prevents NameError for typehints
+from pathlib import Path
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as Expected
+from selenium.common.exceptions import (
+    TimeoutException,
+    UnexpectedAlertPresentException,
+    SessionNotCreatedException,
+)
+
+
+class Driver:
+    """A Selenium webdriver used to operate a Chrome browser"""
+
+    EXCEPTIONS = [
+        TimeoutException,
+        UnexpectedAlertPresentException,
+        SessionNotCreatedException,
+    ]
+
+    def __init__(self, driver_path: Path, download_dir: Path) -> None:
+        """Inits the Driver class with specific download directory
+
+        Parameters
+        ----------
+        driver_path: Path
+            Location where the latest version of chromedriver is downloaded
+        download_dir: Path
+            Path to directory where downloads from the browser should be saved
+        """
+        self.driver = self._create_driver(driver_path, download_dir)
+
+    def fill_in(self, element_id: str, content: str) -> None:
+        """Uses the webdriver to fill in a form field with content
+
+        Parameters
+        ----------
+        element_id: str
+            Id attribute of the form field to fill in with data
+        content: str
+            The information with which to fill in the form field
+        """
+        return self.driver.find_element_by_id(element_id).send_keys(content)
+
+    def click(self, locator: str, loc_type: str = "id") -> None:
+        """Uses the webdriver to locate and click an element
+
+        Parameters
+        ----------
+        locator: str
+            Id or text of the HTML element used to locate and click it
+        loc_type: str, optional
+            The type of locator used to find the element to click. Default is
+            to locate by id but also accepts "link" to locate by link text
+        """
+        if loc_type == "id":
+            return self.driver.find_element_by_id(locator).click()
+        if loc_type == "link":
+            return self.driver.find_element_by_partial_link_text(
+                locator
+            ).click()
+        raise KeyError
+
+    def wait_to_load(
+        self,
+        locator: str,
+        loc_type: str = "id",
+        seconds: int = 45,
+    ) -> None:
+        """Forces the webdriver to wait until an element loads
+
+        Parameters
+        ----------
+        locator: str
+            Id or text of the HTML element used to check that it's loaded
+        loc_type: str, optional
+            The type of locator used to find the element to click. Default is
+            to locate by id but also accepts "link" to locate by link text
+        seconds: int, optional
+            Time to wait for an element to load before raising an error
+        """
+        # set the locator type
+        if loc_type == "id":
+            by = By.ID
+        elif loc_type == "link":
+            by = By.PARTIAL_LINK_TEXT
+        else:
+            raise KeyError
+
+        # try to locate the element within the given number of seconds
+        try:
+            elem_present = Expected.presence_of_element_located((by, locator))
+            WebDriverWait(self.driver, seconds).until(elem_present)
+        except TimeoutException as error:
+            raise error
+
+    def _create_driver(self, driver_path, download_dir) -> webdriver:
+        """Configures a selenium webdriver with a specific download directory
+
+        Parameters
+        ----------
+        driver_path: Path
+            Location where the latest version of chromedriver is downloaded
+        download_dir: Path
+            Path to directory where downloads from the browser should be saved
+
+        Returns
+        -------
+        webdriver
+            Selenium webdriver for Chrome with options configured
+        """
+        # sets default download directory
+        download_dir.mkdir(exist_ok=True, parents=True)
+        options = webdriver.ChromeOptions()
+        prefs = {
+            "download.default_directory": str(download_dir),
+            "download.prompt_for_download": False,
+            "download.directory_upgrade": True,
+            "safebrowsing.enabled": True,
+        }
+        options.add_experimental_option("prefs", prefs)
+
+        # creates driver
+        if not driver_path.exists():
+            message = f"No chromedriver found at '{driver_path}'"
+            raise FileNotFoundError(message)
+        try:
+            driver = webdriver.Chrome(str(driver_path), options=options)
+        except SessionNotCreatedException as error:
+            raise error
+
+        return driver
