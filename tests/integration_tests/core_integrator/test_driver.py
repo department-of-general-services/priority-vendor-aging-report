d624416d4dd67fdebcf5c67f1be0d72793bdadb0
widal001 <williamdaly422@gmail.com>
2021-09-15 09:41:54 -0400

    Adds tests for Driver.wait_to_load() and Driver.fill_in()

--- tests/integration_tests/core_integrator/test_driver.py
+++ tests/integration_tests/core_integrator/test_driver.py
@@ -21,11 +21,10 @@ class TestDriver:
         - Driver.driver is an instance of webdriver.Chrome
         - And that it triggers a download to the specified directory
         """
-        ## setup
+        # setup
         download_dir = test_archive_dir / "core_integrator"
         download_dir.mkdir(exist_ok=True, parents=True)
         assert not any(download_dir.iterdir())
-
         # execution
         try:
             driver.get(URL)
@@ -34,7 +33,6 @@ class TestDriver:
             time.sleep(3)
         except Exception as err:
             raise err
-
         # validation
         assert isinstance(driver, Driver)
         assert any(download_dir.iterdir()) is True
@@ -47,7 +45,6 @@ class TestDriver:
         driver_path = Path.cwd() / "fake"
         assert driver_path.exists() is False
         settings.chrome_driver_path = driver_path
-
         # executive
         with pytest.raises(FileNotFoundError):
             Driver(config=settings)
@@ -63,11 +60,9 @@ class TestClick:
         # setup
         url_out = "https://pypi.org/project/selenium/#description"
         link = "Project description"
-
         # execution
         driver.get(URL)
         driver.click(link, loc_type="link")
-
         # validation
         assert driver.current_url == url_out
 
@@ -78,11 +73,9 @@ class TestClick:
         # setup
         url_out = "https://pypi.org/project/selenium/#description"
         link_id = "description-tab"
-
         # execution
         driver.get(URL)
         driver.click(link_id)
-
         # validation
         assert driver.current_url == url_out
 
@@ -95,7 +88,6 @@ class TestClick:
             driver.click("link_text", loc_type="text")
 
 
-@pytest.mark.skip
 class TestFillIn:
     """Tests the Driver.fill_in() method"""
 
@@ -104,65 +96,63 @@ class TestFillIn:
         # setup
         search_id = "search"
         value_in = "FooBar"
-
         # execution_id
         driver.get(URL)
-        driver.fill_in(driver, search_id, value_in)
-        value_out = driver.find_element_by_id(search_id).get_attribute("value")
-
+        driver.fill_in(search_id, value_in)
+        value_out = driver.driver.find_element_by_id(search_id).get_attribute(
+            "value"
+        )
         # validation
         assert value_in == value_out
-        driver.quit()
 
 
-@pytest.mark.skip
 class TestWaitToLoad:
     """Tests the Driver.wait_to_load() method"""
 
     def test_id_present(self, driver):
-        """Tests the id parameter for Driver.wait_to_load()"""
+        """Tests that the driver successfully exits the wait_to_load() loop if
+        an element with that id is loaded on the page
+        """
         # setup
         search_id = "search"
         # execution
         driver.get(URL)
-        driver.wait_to_load(driver, search_id, seconds=2)
-        search = driver.find_element_by_id(search_id)
+        driver.wait_to_load(search_id, seconds=2)
+        search = driver.driver.find_element_by_id(search_id)
         # validation
         assert search is not None
-        driver.quit()
 
     def test_link_present(self, driver):
-        """Tests the link parameter of Driver.wait_to_load()"""
+        """Tests that the driver successfully exits the wait_to_load() loop if
+        an element with that partial link text is loaded on the page
+        """
         # setup
         link = "Project description"
-
         # execution
         driver.get(URL)
-        driver.wait_to_load(driver, link, type="link", seconds=2)
-        description = driver.find_element_by_partial_link_text(link)
-
+        driver.wait_to_load(link, loc_type="link", seconds=2)
+        description = driver.driver.find_element_by_partial_link_text(link)
         # validation
         assert description is not None
-        driver.quit()
 
     def test_id_missing(self, driver):
-        """Tests that wait_to_load() fails as expected when the id is missing"""
+        """Tests that the wait_to_load() method raises an TimeoutException if
+        the item with that id still isn't present after x number of seconds
+        """
         # setup
         search_id = "fake_id"
-
         # execution
         driver.get(URL)
         with pytest.raises(TimeoutException):
-            driver.wait_to_load(driver, search_id, seconds=1)
-        driver.quit()
+            driver.wait_to_load(search_id, seconds=1)
 
     def test_link_missing(self, driver):
-        """Tests that wait_to_load() fails as expected when the link is missing"""
+        """Tests that the wait_to_load() method raises an TimeoutException if
+        the item with that link text isn't present after x number of seconds
+        """
         # setup
         link = "fake_link"
-
         # execution
         driver.get(URL)
         with pytest.raises(TimeoutException):
-            driver.wait_to_load(driver, link, type="link", seconds=1)
-        driver.quit()
+            driver.wait_to_load(link, loc_type="link", seconds=1)
