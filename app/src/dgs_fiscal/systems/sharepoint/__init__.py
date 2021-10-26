373e417ee014dc797039f1155e68404c1047880a
widal001 <williamdaly422@gmail.com>
2021-10-26 13:37:37 -0400

    Adds BatchedChanges dataclass and batch_upsert() method to list.py

--- app/src/dgs_fiscal/systems/sharepoint/__init__.py
+++ app/src/dgs_fiscal/systems/sharepoint/__init__.py
@@ -1,3 +1,4 @@
-__all__ = ["SharePoint"]
+__all__ = ["SharePoint", "BatchedChanges"]
 
 from dgs_fiscal.systems.sharepoint.client import SharePoint
+from dgs_fiscal.systems.sharepoint.list import BatchedChanges
