47fb9e726342cb640a7275a707f64a7a77dbdebf
widal001 <williamdaly422@gmail.com>
2021-11-10 11:18:27 -0500

    Adds dummy data and tests for PromptPayment.get_new_report()

--- app/tests/integration_tests/prompt_payment/test_prompt_payment.py
+++ app/tests/integration_tests/prompt_payment/test_prompt_payment.py
@@ -1,7 +1,10 @@
+from pathlib import Path
+
 import pytest
 import pandas as pd
 
 from dgs_fiscal.etl import PromptPayment
+from dgs_fiscal.etl.prompt_payment import constants
 from dgs_fiscal.systems import CoreIntegrator, SharePoint
 
 
@@ -11,9 +14,18 @@ def fixture_prompt_payment(test_archive_dir):
     return PromptPayment(local_archive=test_archive_dir)
 
 
+def new_report_file():
+    """Returns the link to a dummy version of the report scraped from
+    CoreIntegrator by CoreIntegrator.scrape_report()
+    """
+    current_dir = Path(__file__).parent
+    return current_dir / "new_report_input.xlsx"
+
+
 class TestPromptPayment:
     """Tests the PromptPayment etl class"""
 
+    @pytest.mark.skip
     def test_init(self, test_prompt, test_archive_dir):
         """Tests that the PromptPayment inits correctly"""
         # validation
@@ -21,6 +33,7 @@ class TestPromptPayment:
         assert isinstance(test_prompt.core_integrator, CoreIntegrator)
         assert isinstance(test_prompt.sharepoint, SharePoint)
 
+    @pytest.mark.skip
     def test_get_old_report(self, test_prompt):
         """Tests that the get_old_report() method executes successfully
 
@@ -36,3 +49,27 @@ class TestPromptPayment:
         # validation
         assert isinstance(df, pd.DataFrame)
         assert len(df[~df["Prompt Payment"]]) == 0
+
+    def test_get_new_report(self, test_prompt, monkeypatch):
+        """Tests that the get_new_report() method executes successfully
+
+        Validates the following conditions:
+        - A dataframe is returned as the result
+        - The columns have been renamed correctly
+        - There are no invoices on the list where the PromptPaymentReport
+          field is False
+        """
+        # setup
+        columns = list(constants.NEW_REPORT["columns"].values())
+        monkeypatch.setattr(
+            test_prompt.core_integrator,
+            "scrape_report",
+            new_report_file,
+        )
+        vendor_ids = ["00048571", "00048571", "00001928", "01012092"]
+        # execution
+        df = test_prompt.get_new_report()
+        # validation
+        assert isinstance(df, pd.DataFrame)
+        assert list(df.columns) == columns
+        assert list(df["Vendor ID"]) == vendor_ids
