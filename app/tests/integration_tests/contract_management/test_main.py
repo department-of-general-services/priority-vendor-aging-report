dc568c1c4053adb9a7fdf64dd559db9a643653ee
widal001 <williamdaly422@gmail.com>
2021-11-15 15:39:54 -0500

    test(contract-mgmt): Adds integration test for get_citibuy_data() method
    Adds the test TestContractManagement.test_get_citibuy_data() which tests 
    the execution of ContractManagement.get_citibuy_data()

--- app/tests/integration_tests/contract_management/test_main.py
+++ app/tests/integration_tests/contract_management/test_main.py
@@ -1,8 +1,7 @@
 import pytest
 
 from dgs_fiscal.etl import ContractManagement
-from dgs_fiscal.etl.contract_management import ContractData
-from tests.integration_tests.contract_management import data
+from dgs_fiscal.etl.contract_management import ContractData, constants
 
 
 @pytest.fixture(scope="session", name="mock_contract")
@@ -22,3 +21,23 @@ class TestContractManagement:
         records = mock_contract.citibuy.get_purchase_orders().records
         # validation
         assert len(records) == 5
+
+    def test_get_citibuy_data(self, mock_contract):
+        """Tests the get_citibuy_data() method executes correctly
+
+        Validates the following conditions:
+        - The return type is ContractData
+        - The columns of ContractData.po match the constants
+        - The columns of ContractData.vendor match the constants
+        - The dataframe in ContractData.vendor has been deduped
+        """
+        # setup
+        po_cols = list(constants.CITIBUY["po_cols"].values())
+        ven_cols = list(constants.CITIBUY["vendor_cols"].values())
+        # validation
+        output = mock_contract.get_citibuy_data()
+        # validation
+        assert isinstance(output, ContractData)
+        assert list(output.po.columns) == po_cols
+        assert list(output.vendor.columns) == ven_cols
+        assert len(output.vendor) == 2
