f47a0a3d194a609cb78a55eadbde86224471f3ea
widal001 <williamdaly422@gmail.com>
2021-11-15 15:38:16 -0500

    feat(contract-mgmt): Implements ContractManagement.get_citibuy_data()
    Implements the get_citibuy_data() method and adds a dataclass called 
    ContractData that serves as the return type for that method

---
+++ app/src/dgs_fiscal/etl/contract_management/constants.py
@@ -0,0 +1,23 @@
+CITIBUY = {
+    "po_cols": {
+        "po_nbr": "PO Number",
+        "release_nbr": "Release Number",
+        "vendor_id": "Vendor ID",
+        "status": "PO Status",
+        "agency": "PO Agency",
+        "cost": "Actual Cost",
+        "date": "PO Date",
+        "contract_agency": "Contract Agency",
+        "dollar_limit": "Contract Dollar Limit",
+        "dollar_spent": "Contract Amount Spent",
+        "start_date": "Contract Start Date",
+        "end_date": "Contract End Date",
+    },
+    "vendor_cols": {
+        "vendor_id": "Vendor ID",
+        "name": "Vendor Name",
+        "contact": "Contact",
+        "email": "Email",
+        "phone": "Phone",
+    },
+}
