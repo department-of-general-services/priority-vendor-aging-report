cd69940688d15542736ee0b3d8d64552e428b6c8
widal001 <williamdaly422@gmail.com>
2022-01-13 15:24:34 -0500

    feat: creates scaffolding for aging_report sub-package
    Creates the scaffolding for a dgs_fiscal.etl.aging_report sub-package 
    with an AgingReport ETL class

---
+++ app/src/dgs_fiscal/etl/aging_report/main.py
@@ -0,0 +1,54 @@
+from __future__ import annotations  # prevents NameError for typehints
+from typing import Optional
+
+import pandas as pd
+from O365.drive import File
+
+from dgs_fiscal.systems import CitiBuy, SharePoint
+
+
+class AgingReport:
+    """Runs the Aging Report workflow, which gets the status of invoices from
+    CitiBuy, CoreIntegrator, and Integrify and updates them in SharePoint
+
+    Attributes
+    ----------
+    citibuy: CitiBuy
+        An instance of the CitiBuy class which manages connection to the
+        CitiBuy database
+    sharepoint: SharePoint
+        An instance of the SharePoint class which manages graph API calls to
+        SharePoint resources
+    """
+
+    def __init__(self, citibuy_url: str) -> None:
+        """Inits the AgingReport class"""
+        self.citibuy = CitiBuy(conn_url=citibuy_url)
+        self.sharepoint = SharePoint()
+
+    def get_citibuy_data(self) -> pd.DataFrame:
+        """Gets a list of open and recently paid invoices from CitiBuy so they
+        can be uploaded to SharePoint and used to build the AgingReport
+
+        Returns
+        -------
+        pd.DataFrame
+            A dataframe of the invoices exported from CitiBuy
+        """
+        pass
+
+    def upload_invoice_data(self, folder_id: Optional[str] = None) -> File:
+        """Uploads CitiBuy invoice data to SharePoint as an Excel file
+
+        Parameters
+        ----------
+        folder_id: str, optional
+            The id of the folder to which the invoice data will be uploaded
+
+        Returns
+        -------
+        File
+            Returns an instance of the O365 File class for the Excel file that
+            was created in the SharePoint folder passed to this
+        """
+        pass
